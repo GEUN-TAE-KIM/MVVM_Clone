2289694cd6c0cd3e77b37fa5088104a7ac60818b
rmsxo5678 <rmsxo5678@naver.com>
2023-01-25 12:19:09 +0900

    retrofit 시작 설정

--- app/src/main/java/jp/co/archive_asia/mvvm_udemy_clone/ui/MainActivity.kt
+++ app/src/main/java/jp/co/archive_asia/mvvm_udemy_clone/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package jp.co.archive_asia.mvvm_udemy_clone
+package jp.co.archive_asia.mvvm_udemy_clone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import jp.co.archive_asia.mvvm_udemy_clone.R
 import jp.co.archive_asia.mvvm_udemy_clone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/jp/co/archive_asia/mvvm_udemy_clone/ui/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/jp/co/archive_asia/mvvm_udemy_clone/ui/foodjoke/FoodJokeFragment.kt
@@ -1,10 +1,11 @@
-package jp.co.archive_asia.mvvm_udemy_clone
+package jp.co.archive_asia.mvvm_udemy_clone.ui.foodjoke
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import jp.co.archive_asia.mvvm_udemy_clone.R
 
 class FoodJokeFragment : Fragment() {
 
